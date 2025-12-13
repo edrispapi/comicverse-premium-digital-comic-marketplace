c2aac16fbb69d846c5a60942cfe022afc461eccd
Vibesdk <vibesdk-bot@cloudflare.com>
2025-12-13 13:02:43 +0000

    fix: Add DialogTitle and DialogDescription to CommandDialog in ui/command.tsx to resolve Radix UI a11y warnings and console spam on HomePage Navbar search

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -31,6 +31,12 @@ const CommandDialog = ({ children, ...props }: DialogProps) => {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0">
+        {/* Accessibility header required by Radix Dialog */}
+        <DialogHeader className="p-0 mb-2">
+          <DialogTitle>Search Comics...</DialogTitle>
+          <DialogDescription>Type a command or search for comics</DialogDescription>
+        </DialogHeader>
+
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
